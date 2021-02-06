376982ac9ab7cc75040aa03f2f990493b4f061c9
Dmitri Larin <Diman-Larin@mail.ru>
2021-02-06 14:15:52 +0400

    Вынес randomNum за пределы getNu

--- temp.js
+++ temp.js
@@ -6,14 +6,13 @@ let game = function() {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
-    //console.log(getRandomInt(1, 100));
     
-    //let randomNum = Number(getRandomInt());
+    let randomNum = getRandomInt(1, 100);
 
     function getNum(){
         
         let question = +prompt('Угадай число от 1 до 100');
-        let randomNum = getRandomInt(1, 100);
+        
 
         if (isNaN(question)) {
             alert('Введите число!');
